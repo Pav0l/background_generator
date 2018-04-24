c25c3796ba84acf07c32de70e95a783b2e2f5be4
pavol.karas@gmail.com <pavol.karas@gmail.com>
2018-04-24 20:28:10 +0200

    optimized JS code, added textContent

--- script.js
+++ script.js
@@ -3,21 +3,17 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-
-color1.addEventListener("input", () => {
-	body.style.background = 
+let setGradient = () => {
+body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
 	+ ")";
-});
 
-color2.addEventListener("input", () => {
-		body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-}) ;
+css.textContent = body.style.background + ";";
+};
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
